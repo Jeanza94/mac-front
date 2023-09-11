6f589a9e6fa23faf2e36a88b42366a1f00f65237
Jean Zapata <jeanza94@hotmail.com>
2023-09-11 17:40:24 -0500

    fontawesome icons instalado, estilos botones 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 import { Button } from "./components/buttons"
 import { Input } from "./components/inputs"
 
@@ -19,7 +20,7 @@ const App = () => {
         información
       </h3>
 
-      <form className="flex gap-4">
+      <form className="flex flex-wrap gap-4 items-end">
         <label className="flex flex-col gap-1">
           <span>Tipo de documento</span>
           <Input
@@ -39,11 +40,20 @@ const App = () => {
         <Button 
           text="buscar"
           title="Buscar persona"
+          type="submit"
+          className="bg-blue-950 hover:bg-blue-600 focus:bg-blue-600"
+          spanClassName="text-white font-medium"
+          icon={faMagnifyingGlass}
+          iconClassName="text-white text-sm"
         />
 
         <Button 
           text="busqueda avanzada"
           title="Abrir opciones de busqueda avanzada"
+          className="border-blue-950 hover:bg-blue-300 focus:bg-blue-300"
+          spanClassName="text-blue-600 font-medium group-hover:text-white group-focus:text-white"
+          icon={faMagnifyingGlass}
+          iconClassName="text-blue-600 group-hover:text-white group-focus:text-white"
         />
       </form>
     </main>
--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,25 +1,56 @@
-import {FC} from 'react'
+import { FC } from 'react'
 import { twMerge } from 'tailwind-merge';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface PropsButton {
   className?: string,
-  text:string
-  title: string 
+  icon?: IconDefinition,
+  iconClassName?: string,
+  spanClassName?: string,
+  text: string,
+  title: string,
   type?: "submit" | "button",
   onClick?: () => void,
 }
 
-export const Button:FC<PropsButton> = ({title, className, onClick, type="button", text}) => {
+export const Button: FC<PropsButton> = ({
+  title,
+  className,
+  onClick,
+  type = "button",
+  text,
+  spanClassName,
+  icon,
+  iconClassName
+}) => {
   return (
     <button
       title={title}
       onClick={onClick}
       type={type}
       className={twMerge(`
-        border-2 h-10 text-sm uppercase px-4 rounded-lg transition-colors duration-300 ease-in-out
+        border-2 h-10  px-4 rounded-lg transition-colors duration-300 ease-in-out flex
+        items-center gap-4 group focus:outline-none
       `, className)}
     >
-      {text}
+      <span
+        className={twMerge(`
+          text-sm uppercase transition-colors duration-300 ease-in-out
+        `, spanClassName)}
+      >
+        {text}
+      </span>
+      {
+        icon && (
+          <FontAwesomeIcon
+            icon={icon}
+            className={twMerge(`
+              transition-colors duration-300 ease-in-out
+            `, iconClassName)}
+          />
+        )
+      }
     </button>
   )
 }
