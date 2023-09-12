940f5f548903ef10aaf85a4e0f05b7ca962a13b3
Jean Zapata <jeanza94@hotmail.com>
2023-09-12 02:55:41 -0500

    eliminado tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { UserProvider } from './context/users';
 import App from './App.tsx'
 import './index.css'
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './tanstack/config.ts';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <UserProvider>
         <App />
       </UserProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
   </React.StrictMode>,
 )
