942466be5a26cea1a69f9d8f512250d2c104a700
Jean Zapata <jeanza94@hotmail.com>
2023-09-12 07:56:29 -0500

    actualizacion de index, despliegue en netlify

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = "http://127.0.0.1:8000/"
+// const url = "http://127.0.0.1:8000/"
+const url = "https://mac-backend-zsmf.onrender.com/"
 
 export const macApi = axios.create({
   baseURL: url
